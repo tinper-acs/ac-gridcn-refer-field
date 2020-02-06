9db20a234468a3e07acbdce65201654fdcb4dc87
liushaozhen <804199994@qq.com>
2020-02-06 15:12:22 +0800

    feat:

---
+++ build/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _GridcnReferField = require('./GridcnReferField');
+
+var _GridcnReferField2 = _interopRequireDefault(_GridcnReferField);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+exports["default"] = _GridcnReferField2["default"];
+module.exports = exports['default'];
